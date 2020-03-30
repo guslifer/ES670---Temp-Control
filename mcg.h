ecf05a4f7dfda9cc638e0a7d5d356030915aff64
guslifer <gustavolinofernandes@gmail.com>
2020-03-29 22:30:13 -0300

    Demais Arquivos
    Aula 8 - Inserção dos demais arquivos que utilizaremos no projeto

---
+++ mcg.h
@@ -0,0 +1,23 @@
+/* ***************************************************************** */
+/* File name:        mcg.h                                           */
+/* File description: Header file containing the functions/methods    */
+/*                   interfaces for handling the Multipurpose clock  */
+/*                   generator module                                */
+/*                                                                   */
+/* Author name:      dloubach                                        */
+/* Creation date:    21out2015                                       */
+/* Revision date:    25fev2016                                       */
+/* ***************************************************************** */
+
+#ifndef SOURCES_MCG_H_
+#define SOURCES_MCG_H_
+
+/* ************************************************ */
+/* Method name:        mcg_clockInit                */
+/* Method description: main board clk configuration */
+/* Input params:       n/a                          */
+/* Output params:      n/a                          */
+/* ************************************************ */
+void mcg_clockInit(void);
+
+#endif /* SOURCES_MCG_H_ */
