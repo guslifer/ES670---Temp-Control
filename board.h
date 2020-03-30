680ae3e85da65dd5299b7a209037fb2020a08998
guslifer <62360861+guslifer@users.noreply.github.com>
2020-03-29 21:58:52 -0300

    Set defines
    Precisa arrumar para não utilizar a bibioteca do KSDK

--- board.h
+++ board.h
@@ -25,4 +25,51 @@
 #define BLUE_LED_GPIO_BASE_PNT   PTD   /* peripheral gpio base pointer */
 #define BLUE_LED_PIN	         (uint32_t) 1u
 
+
+/* Configuração dos set-up para utilziar o LCD
+ *
+ * Quando LCD_RS = LCD_RS_HIGH => Registrador de dados é selecionados
+ * Quando (LCD_RS = LCD_RS_LOW => Registrador de instruções é selecionado.
+ */
+
+#define LCD_PORT_BASE_PNT        PORTC
+#define LCD_GPIO_BASE_PNT        PTC
+
+#define LCD_RS_PIN               8U
+#define LCD_RS_DIR				 kGpioDigitalOutput
+#define LCD_RS_ALT				 kPortMuxAsGpio
+
+#define LCD_ENABLE_PIN			 9U
+#define LCD_ENABLE_DIR			 kGipioDigitalOutput
+#define LCD_ENABLE_ALT			 kPortMuxAsGpio
+
+#define LCD_RS_HIGH				 1U
+#define LCD_RS_DATA				 LCD_RS_HIGH
+
+#define LCD_RS_LOW				 0U
+#define LCD_RS_CMD				 LCD_RS_LOW
+
+#define LCD_ENABLED				 1U
+#define LCD_DISABLED			 0U
+
+#define LCD_DATA_DIR			 kGpioDigitalOutput
+#define LCD_DATA_ALT			 kPortMuxAsGpio
+
+#define LCD_DATA_DB0_PIN		 0U
+#define LCD_DATA_DB1_PIN		 1U
+#define LCD_DATA_DB2_PIN		 2U
+#define LCD_DATA_DB3_PIN		 3U
+#define LCD_DATA_DB4_PIN		 4U
+#define LCD_DATA_DB5_PIN		 5U
+#define LCD_DATA_DB6_PIN		 6U
+#define LCD_DATA_DB7_PIN		 7U
+
+/* Final das definições do LCD para a placa */
+
+
+
+
+
+
+
 #endif /* SOURCES_BOARD_H_ */
