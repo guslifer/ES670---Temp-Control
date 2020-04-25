bef9463a1b6fb0bdd8e2bec82dcc05e49b2cf058
guslifer <gustavolinofernandes@gmail.com>
2020-04-25 13:47:22 -0300

    Adequação main.c
    Formatação basicamente

--- Aula Pratica 6/main.c
+++ Aula Pratica 6/main.c
@@ -12,55 +12,19 @@
 
 /* Incluindo bibliotecas */
 #include "board.h"  
-#include "lptmr.h"
-#include "display7seg.h"
-
-
-/* Variaveis globais */
-
-/* Strings para testar display LCD */
-unsigned int ui4ms = 4; 
-unsigned char refresh = 0;
-
-
-
-/*configure cyclic executive interruption*/
-
-
-
-void d7s_segurador(void)
-{
-    refresh = 1;
-    return;
-}
-
-
-void tc_installLptmr0(ui4ms, d7s_segurador);
-
-/* **************************************************************************** */
-/* Nome do metodo:          boardInit                                           */
-/* DescriÃƒÂ§ÃƒÂ£o:               Inicializa os parametros necessarios para o   */
-/*                          teste                                               */
-/*                                                                              */
-/* Parametros de entrada:   n/a                                                 */
-/*                                                                              */
-/* Parametros de saida:     n/a                                                 */
-/* **************************************************************************** */
-
 
 
 void boardInit(void)
 {
-    /* inicializando o D7S */
-	display7seg_init();
+    boardInit();
 }
 
 
 
 /* **************************************************************************** */
 /* Nome do metodo:          main                                                */
-/* DescriÃƒÂ§ÃƒÂ£o:         Escreve um número de 4 digitos usando os D7S        */
-/*                          utilizando as interrupções para manter visivel      */
+/* DescriÃƒÂ§ÃƒÂ£o:         Inicializa o aquecedor e o cooler com 50% da tensão */
+/*                                                                              */
 /*                                                                              */
 /* Parametros de entrada:   n/a                                                 */
 /*                                                                              */
@@ -73,7 +37,6 @@ int main(void)
     * Configuração da modulação PWM 
     * Inicialização do cooler 
     * Inicialização do aquecedor */
-    boardInit();
     PWM_init();
     coolerfan_init();
     heater_init();
