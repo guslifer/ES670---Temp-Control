36fcd43d3525af30cc9774ddb06c6eeed84a9df7
guslifer <gustavolinofernandes@gmail.com>
2020-08-01 16:48:18 -0300

    Sintonia de PID - ZN
    arquivos do matlab para escolher os ganhos do controlador

--- Projeto Final/Final_v1/Sintonia_PID.m
+++ Projeto Final/Final_v1/Sintonia_PID.m
@@ -1,30 +1,30 @@
-t = temp_out.time; 
-temp = temp_out.data;
+%%Encontra a tangente da resposta do sistema ao degrau
 
-plot(t, temp); 
-fy = interp1(t,temp,0:0.01:30)
-y = fy;
-tm = 0:30/(length(fy)-1):30;
-plot(tm, fy);
-d1y = gradient(y,tm);                                            % Numerical Derivative
-d2y = gradient(d1y,tm);                                          % Numerical Second Derivative
-t_infl = interp1(d1y, tm, max(d1y));                             % Find ‘t’ At Maximum Of First Derivative
-y_infl = interp1(tm, y, t_infl);                                 % Find ‘y’ At Maximum Of First Derivative
-slope  = interp1(tm, d1y, t_infl);                               % Slope Defined Here As Maximum Of First Derivative
-intcpt = y_infl - slope*t_infl;                                 % Calculate Intercept
-tngt = slope*tm + intcpt;                                        % Calculate Tangent Line
+t = temp_out.time;
+y = temp_out.data;
 
+yp = gradient(y,t) 
 
-figure(1)
-plot(tm, y)
+[m, p] = max(yp)
+
+tang = yp(p)*(t-t(p))+y(p)
+plot(t,y)
 hold on
-plot(tm, fy)
-% plot(t, d1y, '-.m',    t, d2y, '--c')                           % Plot Derivatives (Optional)
-plot(tm, tngt, '-r', 'LineWidth',1)                              % Plot Tangent Line
-% xlim([0 4])
-% ylim([0 1])
-% plot(t_infl, y_infl, 'bp')                                      % Plot Maximum Slope
-hold off
-grid
-% legend('y(t)', 'y(t) Fit', 'dy/dt', 'd^2y/dt^2', 'Tangent', 'Location','E')
-% axis([xlim    min(min(y),intcpt)  ceil(max(y))])
+plot(tm, tang)
+plot(t(p),y(p),'ro')
+
+
+L_pos = knnsearch(tang, 0)
+L = y(L_pos)
+K = max(y)
+T_pos = knnsearch(tang, K)
+T = t(T_pos) - t(L_pos)
+
+plot(t(T_pos),tang(T_pos),'ro')
+
+
+
+
+
+
+
